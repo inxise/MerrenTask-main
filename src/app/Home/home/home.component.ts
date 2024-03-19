d4cf8f240596cb66e5b8e9c38208b4b4d150a4bc
Sandeep Khode <sgkhode@gmail.com>
2024-03-19 16:00:38 +0530

    first commit

---
+++ src/app/Home/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../Services/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+
+export class HomeComponent implements OnInit{
+  objects: any[]=[];
+  newObject: any = { id: '', name: '', data: { price: null, color: '' } };
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.fetchObjects();
+  }
+
+  fetchObjects(): void {
+    this.apiService.getObjects().subscribe(
+      (response: any[]) => {
+        this.objects = response;
+      },
+      (error) => {
+        console.error('Error fetching objects:', error);
+      }
+    );
+  }
+
+  addObject(): void {
+    this.apiService.addObject(this.newObject).subscribe(
+      (response: any) => {
+        console.log('Object added successfully:', response);
+        this.objects.push(this.newObject)
+        //this.newObject = { id: '', name: '', data: { price: null, color: '' } };
+      },
+      (error) => {
+        console.error('Error adding object:', error);
+      }
+    );
+  }
+}
